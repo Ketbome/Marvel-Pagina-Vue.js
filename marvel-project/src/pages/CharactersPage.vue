de8c481b07808b349229f6ff9a547d3b6f558b7c
Ketbome <pims.2711@gmail.com>
2023-05-24 21:16:12 -0400

    feat: :zap: Actualizacion en pag de personajes y comics

--- marvel-project/src/pages/CharactersPage.vue
+++ marvel-project/src/pages/CharactersPage.vue
@@ -1,30 +1,42 @@
 <template>
-    <div>
-      <h2>Personajes</h2>
-      <ul>
-        <li v-for="character in characters" :key="character.id">
-          {{ character.name }}
-        </li>
-      </ul>
-    </div>
-  </template>
-  
-  <script>
-  import { reactive, onMounted } from 'vue';
-  
-  export default {
-    setup() {
-      const characters = reactive([]);
-  
-      onMounted(() => {
-        // Realiza la solicitud a la API de Marvel para obtener los personajes y actualiza el arreglo characters
-        // characters.push(...);
-      });
-  
-      return {
-        characters,
-      };
+  <div>
+    <h2>Personajes</h2>
+    <ul>
+      <li v-for="personaje in personajes" :key="personaje.id">
+        {{ personaje.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      personajes: [],
+    };
+  },
+  created() {
+    this.fetchPersonajes();
+  },
+  methods: {
+    fetchPersonajes() {
+      const ts = '1';
+      const publicKey = '39d77a9d8857c84a2422f18b2de8c1d1';
+      const hash = 'd871bb31390a8b088f14848d63c14333';
+
+      const apiUrl = `http://gateway.marvel.com/v1/public/characters?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
+
+      axios.get(apiUrl)
+        .then(response => {
+          this.personajes = response.data.data.results;
+        })
+        .catch(error => {
+          console.error(error);
+        });
     },
-  };
-  </script>
-  
+  },
+};
+</script>
--- marvel-project/src/pages/ComicsPage.vue
+++ marvel-project/src/pages/ComicsPage.vue
@@ -1,12 +1,42 @@
 <template>
-    <div>
-      <!-- Contenido de la plantilla -->
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    // Opciones del componente
-  }
-  </script>
-  
+  <div>
+    <h2>Comics</h2>
+    <ul>
+      <li v-for="comic in comics" :key="comic.id">
+        {{ comic.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      comics: [],
+    };
+  },
+  created() {
+    this.fetchComics();
+  },
+  methods: {
+    fetchComics() {
+      const ts = '1';
+      const publicKey = '39d77a9d8857c84a2422f18b2de8c1d1';
+      const hash = 'd871bb31390a8b088f14848d63c14333';
+
+      const apiUrl = `http://gateway.marvel.com/v1/public/comics?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
+
+      axios.get(apiUrl)
+        .then(response => {
+          this.comics = response.data.data.results;
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    },
+  },
+};
+</script>
